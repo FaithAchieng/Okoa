f76e202070b6b54cc203cf926232f26d4a3ed17c
FaithAchieng <135111683+FaithAchieng@users.noreply.github.com>
2023-10-05 11:57:40 +0300

    Add files via upload

---
+++ okoa.java
@@ -0,0 +1,193 @@
+package okoa;
+import java.util.Scanner;
+public class okoa {
+
+	public static void main(String[] args) {
+System.out.println("Enter the number of the option you wish to pick");
+Scanner in=new Scanner(System.in);
+int dec1,dec2,dec3,dec4,dec5,dec6,dec7,dec8,dec9,dec10;
+System.out.println("0:Sh20(30mins,3hrs)");
+System.out.println("1:Sh10(15Mins, 1hr)");
+System.out.println("2:Sh20(15Mins, midnight");
+System.out.println("3:Okoa 50");
+System.out.println("4:Okoa 20");
+System.out.println("5:Okoa 10");
+System.out.println("6:Okoa 5");
+System.out.println("7:Okoa 20(900MB, 1hr)");
+System.out.println("8:Okoa Internet");
+System.out.println("98:More");
+int option;
+option=in.nextInt();
+switch(option){
+	case 0:
+		System.out.println("1:Accept 2:Decline 0:Back");
+		dec1=in.nextInt();
+		if(dec1==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		
+		break;
+	case 1:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec2=in.nextInt();
+		if(dec2==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		
+		
+		break;
+	case 2:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec3=in.nextInt();
+		if(dec3==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		break;
+	case 3:
+	System.out.println("1.Accept 2:Decline 0:Back");
+	dec4=in.nextInt();
+	if(dec4==0) {
+		System.out.println("0:Sh20(30mins,3hrs)");
+		System.out.println("1:Sh10(15Mins, 1hr)");
+		System.out.println("2:Sh20(15Mins, midnight");
+		System.out.println("3:Okoa 50");
+		System.out.println("4:Okoa 20");
+		System.out.println("5:Okoa 10");
+		System.out.println("6:Okoa 5");
+		System.out.println("7:Okoa 20(900MB, 1hr)");
+		System.out.println("8:Okoa Internet");
+		System.out.println("98:More");
+	}
+	
+	break;
+	case 4:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec5=in.nextInt();
+		if(dec5==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		break;
+	case 5:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec6=in.nextInt();
+		if(dec6==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		break;
+	case 6:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec7=in.nextInt();
+		if(dec7==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		
+		break;
+	case 7:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec8=in.nextInt();
+		if(dec8==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		break;
+	case 8:
+		System.out.println("1.Accept 2:Decline 0:Back");
+		dec9=in.nextInt();
+		if(dec9==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		break;
+	case 98:
+		System.out.println("9: Okoa Minutes 10: Okoa SMS 11: Lipa Okoa #: Check Balance 0:Back");
+		dec10=in.nextInt();
+		if(dec10==0) {
+			System.out.println("0:Sh20(30mins,3hrs)");
+			System.out.println("1:Sh10(15Mins, 1hr)");
+			System.out.println("2:Sh20(15Mins, midnight");
+			System.out.println("3:Okoa 50");
+			System.out.println("4:Okoa 20");
+			System.out.println("5:Okoa 10");
+			System.out.println("6:Okoa 5");
+			System.out.println("7:Okoa 20(900MB, 1hr)");
+			System.out.println("8:Okoa Internet");
+			System.out.println("98:More");
+		}
+		
+		default:
+			System.out.println("Invalid option");
+}
+
+	}
+
+}
